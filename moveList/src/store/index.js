e3a981de5278d89640c424fbf98cc77c7790c2dd
bigliuliu <1552000425@qq.com>
2024-08-09 17:10:08 +0800

    destruct store directory

--- moveList/src/store/index.js
+++ moveList/src/store/index.js
@@ -1,47 +1,11 @@
-import { configureStore, createSlice,createAction } from "@reduxjs/toolkit";
-export const reset =createAction('app/reset')
-const movesSlice = createSlice({
-  name: "movie",
-  initialState: [],
-  reducers: {
-    addMovie(state, action) {
-      state.push(action.payload);
-    },
-    removeMovie(state, action) {
-      const index = state.indexOf(action.payload);
-      state.splice(index, 1);
-    },
-  },
-  extraReducers(builder){
-    builder.addCase(reset,(state,action)=>{
-        return []
-    })
-  }
-});
+import { configureStore } from "@reduxjs/toolkit";
+import { songsReducer,addSong,removeSong} from "./slices/songsSlice";
+import { movieReducer,addMovie,removeMovie} from "./slices/moviesSlice";
 
-const songsSlice = createSlice({
-  name: "song",
-  initialState: [],
-  reducers: {
-    addSong(state, action) {
-      state.push(action.payload);
-    },
-    removeSong(state, action) {
-      // action.payload === string, the song we want to delete
-      const index = state.indexOf(action.payload);
-      state.splice(index, 1);
-    },
-  },
-  extraReducers(builder){
-    builder.addCase(reset,(state, action)=>{
-        return [];
-    })
-  }
-});
 const store = configureStore({
   reducer: {
-    songs: songsSlice.reducer,
-    movies: movesSlice.reducer,
+    songs: songsReducer,
+    movies: movieReducer,
   },
 });
 // const startingState = store.getState()
@@ -50,5 +14,5 @@ const store = configureStore({
 // const finalState = store.getState();
 // console.log(JSON.stringify(finalState))
 export { store };
-export const { addSong, removeSong } = songsSlice.actions;
-export const { addMovie, removeMovie } = movesSlice.actions;
+export { addSong, removeSong,addMovie,removeMovie } 
+
