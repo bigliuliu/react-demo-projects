9f128ee98e2afd9b41f6a71f7e9a586b2d9dfdc8
bigliuliu <1552000425@qq.com>
2024-08-09 16:16:10 +0800

    use extraREducers to reset both of states

--- moveList/src/store/index.js
+++ moveList/src/store/index.js
@@ -1,1 +1,52 @@
-import {} 
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const movesSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+    reset(state, action) {
+      return [];
+    },
+  },
+});
+
+const songsSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {
+      // action.payload === string, the song we want to delete
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+  extraReducers(builder){
+    builder.addCase(movesSlice.actions.reset,(state, action)=>{
+        return [];
+    })
+  }
+});
+const store = configureStore({
+  reducer: {
+    songs: songsSlice.reducer,
+    movies: movesSlice.reducer,
+  },
+});
+// const startingState = store.getState()
+// console.log(JSON.stringify(startingState))
+// store.dispatch(songsSlice.actions.addSong("0002123"))
+// const finalState = store.getState();
+// console.log(JSON.stringify(finalState))
+export { store };
+export const { addSong, removeSong } = songsSlice.actions;
+export const { addMovie, removeMovie, reset } = movesSlice.actions;
